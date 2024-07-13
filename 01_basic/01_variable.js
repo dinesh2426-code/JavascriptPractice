21e789a96d7ee2e0dcbd9b4f9ef4e53fe03cd16b
dinesh2426-code <dinesh252630@gmail.com>
2024-07-13 10:05:10 +0000

    datatypes

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -3,8 +3,9 @@ let accountEmail = "dinesh@gamail,com"
 var accountPassword = "12345"
 accountCity = "Jalna"
 
+let date;
 // accountId = 2   not allowed
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,date])
 
 // prefer not to use  var because of issue of block scope and funcctional scope 
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,12 @@
+//number =>2 to the power 53
+//bigint 
+//string => ""
+//boolean => true/ false
+//null => standalone value  type is object
+//undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "dinesh")
+
